dc420aa14d5f6ba1bbb6638167e8d4caa12851fb
Spandan Bemby <spandan.777@gmail.com>
2019-09-22 09:50:00 -0700

    include ascii_tree dep

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(name='call_graph',
       version='0.1',
       description='create call-graph',
-      python_requires='>=3'
+      python_requires='>=3',
+      dependency_links=['https://github.com/spandanb/ascii_tree/tarball/master#egg=package-0.1'
 )
